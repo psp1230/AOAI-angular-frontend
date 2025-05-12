cecf4d446d93516cf6b554b2f54752af3fcc6f1e
beibei3030 <a0911235479@gmail.com>
2025-05-12 21:21:48 +0800

    新增env環境

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,18 @@
+export const environment = {
+    production: false,
+    api_key: '2YDSTqntN7z40Mj12hHbBUuIQEPdMtcBo4yt5eieY96imNxrywLcJQQJ99ALACHYHv6XJ3w3AAAAACOGjR5o',
+    api_version: '2024-12-01-preview',
+    azure_endpoint: 'https://ai-jamesjao3682ai896846892290.openai.azure.com/',
+    deployment: 'gpt-4o',
+    deployments: [{
+        name: 'gpt-4o',
+        description: 'GPT-4 <div class="small">適合大多數任務</div>',
+        threshold: 10000
+    }],
+    document_serverless_endpoint: '',
+    document_search_endpoint: '',
+    document_threshold: 10000,
+    export_serverless_endpoint: '',
+    website_serverless_endpoint: '',
+    word_serverless_endpoint: ''
+};
